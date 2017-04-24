84b0402e18835556c6126b39411b55be40a94fc4
Michael Dippery <michael@monkey-robot.com>
2017-04-24 12:30:14 -0700

    Define tokens for .ini files

---
+++ src/main/scala/com/mipadi/lang/ini/Token.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017 Michael Dippery <michael@monkey-robot.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mipadi.lang.ini
+
+
+sealed trait Token
+
+case object LBRACE extends Token
+case object RBRACE extends Token
+case object EQUALS extends Token
+case object NEWLINE extends Token
+case class IDENTIFIER(s: String) extends Token
+case class QUOTED(s: String) extends Token
+case class STRING(s: String) extends Token
